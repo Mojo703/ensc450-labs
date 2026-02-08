ed0a77b7c7cdacea185d5be816dcf0bf2b10c2c5
Matthew Goodman <matthewpetersg@gmail.com>
2026-02-07 20:40:48 -0800

    include the clock in waveform.

--- Lab1/sim/scripts/simulate.tcl
+++ Lab1/sim/scripts/simulate.tcl
@@ -1,4 +1,4 @@
 vsim E
 add wave -radix hex {/e/A} {/e/B} {/e/S}
-add wave -radix bin {/e/Cin} {/e/Cout} {/e/Ovfl}
+add wave -radix bin {/e/clk} {/e/Cin} {/e/Cout} {/e/Ovfl}
 restart -f ; run 300 ns
